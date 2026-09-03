287da4981d58695c826547fd7191608e5dc6993e
JohnDexter04 <booljohndexter4@gmail.com>
2026-09-03 20:50:35 +0800

    Add UsersModel, UsersController, users_view, and users route

---
+++ app/models/UsersModel.php
@@ -0,0 +1,13 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+class UsersModel extends Model
+{
+    protected $table = 'users';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+?>
